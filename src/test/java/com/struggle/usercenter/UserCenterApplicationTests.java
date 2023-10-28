4b90158b997b04412199dae1181575a979b03af8
Mr.Chen <2490197735@qq.com>
2023-10-28 17:11:00 +0800

    添加了注册校验功能

--- src/test/java/com/struggle/usercenter/UserCenterApplicationTests.java
+++ src/test/java/com/struggle/usercenter/UserCenterApplicationTests.java
@@ -2,10 +2,21 @@ package com.struggle.usercenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootTest
 class UserCenterApplicationTests {
 
+    @Test
+    void testDigest() throws NoSuchAlgorithmException {
+        String password = DigestUtils.md5DigestAsHex(("struggle" + "myPassword").getBytes());
+        System.out.println(password);
+
+    }
     @Test
     void contextLoads() {
     }
---
+++ src/test/java/com/struggle/usercenter/service/UserServiceTest.java
@@ -0,0 +1,65 @@
+package com.struggle.usercenter.service;
+import java.util.Date;
+
+import com.struggle.usercenter.model.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+
+/**
+ * 用户服务测试
+ * @author Mr.Chen
+ */
+@SpringBootTest
+class UserServiceTest {
+    @Resource
+    private UserService userService;
+    @Test
+    public void testAddUser(){
+        User user = new User();
+        user.setUsername("struggle");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://ebui-cdn.bj.bcebos.com/yiyan-logo.png");
+        user.setUserPassword("122");
+        user.setPhone("11");
+        user.setEmail("222");
+
+        boolean result = userService.save(user);
+        System.out.println(user.getId());
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void userRegister() {
+        String userAccount = "struggle1";
+        String userPassword = "";
+        String checkPassword = "123456";
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1,result);
+        userAccount = "st";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertEquals(-1,result);
+        userAccount = "struggle1";
+        userPassword = "123456";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertEquals(-1,result);
+        userAccount = "str uggle";
+        userPassword = "12345678";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertEquals(-1,result);
+        checkPassword = "123456789";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertEquals(-1,result);
+        userAccount="struggle";
+        checkPassword="12345678";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertEquals(-1,result);
+        userAccount="struggle1";
+        result = userService.userRegister(userAccount,userPassword,checkPassword);
+        Assertions.assertTrue(result>0);
+
+    }
+}
