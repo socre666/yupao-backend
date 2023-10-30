08e5670894b6ea67ee6d9f77c05ccb43fc135f0b
Mr.Chen <2490197735@qq.com>
2023-10-30 18:40:51 +0800

    update

--- src/main/java/com/struggle/usercenter/service/UserService.java
+++ src/main/java/com/struggle/usercenter/service/UserService.java
@@ -3,16 +3,39 @@ package com.struggle.usercenter.service;
 import com.struggle.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
- *用户服务
+ * 用户服务
  */
 public interface UserService extends IService<User> {
+
+
+
     /**
      * 用户注册
-     * @param userAccount 用户账户
-     * @param userPassword 用户密码
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
      * @param checkPassword 校验密码
      * @return 新用户id
      */
-    long userRegister(String userAccount,String userPassword,String checkPassword);
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    User userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户脱敏
+     * @param originUser
+     * @return
+     */
+    User getSafetyUser(User originUser);
 }
--- src/main/java/com/struggle/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/struggle/usercenter/service/impl/UserServiceImpl.java
@@ -5,27 +5,38 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.struggle.usercenter.model.domain.User;
 import com.struggle.usercenter.service.UserService;
 import com.struggle.usercenter.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.struggle.usercenter.contant.UserConstant.USER_LOGIN_STATE;
 
 /**
  *用户服务实现类
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
     @Resource
     private UserMapper userMapper;
+    /**
+     * 盐值，混淆密码
+     */
+    private static final String SALT = "struggle";
+
     @Override
     public long userRegister(String userAccount, String userPassword, String checkPassword) {
         //1，校验
         if(StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
+            //todo 修改为自定义异常
             return -1;
         }
         if(userAccount.length() < 4){
@@ -52,7 +63,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         //2,加密
-        final String SALT = "struggle";
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
         //3，插入数据
         User user = new User();
@@ -63,6 +73,66 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         return user.getId();
+    }
+
+    @Override
+    public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        //1，校验
+        if(StringUtils.isAnyBlank(userAccount,userPassword)){
+            return null;
+        }
+        if(userAccount.length() < 4){
+            return null;
+        }
+        if(userPassword.length()<8){
+            return null;
+        }
+        //账户不能包含特殊字符
+        String regex = "[^a-zA-Z0-9\\\\s]";
+        Matcher matcher = Pattern.compile(regex).matcher(userAccount);
+        if(matcher.find()){
+            return null;
+        }
+        //2,加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        //查询用户是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount",userAccount);
+        queryWrapper.eq("userPassword",encryptPassword);
+        User user = userMapper.selectOne(queryWrapper);
+        //用户不存在
+        if(user == null){
+            log.info("user login failed, userAccount cannot match userPassword");
+            return null;
+        }
+        //3，用户脱敏
+        User safetyUser = getSafetyUser(user);
+        //4，记录用户的登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE,safetyUser);
+
+        return safetyUser;
+
+    }
+
+    /**
+     * 用户脱敏
+     * @param originUser
+     * @return
+     */
+    @Override
+    public User getSafetyUser(User originUser){
+        User safetyUser = new User();
+        safetyUser.setId(originUser.getId());
+        safetyUser.setUsername(originUser.getUsername());
+        safetyUser.setUserAccount(originUser.getUserAccount());
+        safetyUser.setAvatarUrl(originUser.getAvatarUrl());
+        safetyUser.setGender(originUser.getGender());
+        safetyUser.setPhone(originUser.getPhone());
+        safetyUser.setEmail(originUser.getEmail());
+        safetyUser.setUserRole(originUser.getUserRole());
+        safetyUser.setUserStatus(originUser.getUserStatus());
+        safetyUser.setCreateTime(originUser.getCreateTime());
+        return safetyUser;
     }
 }
 
