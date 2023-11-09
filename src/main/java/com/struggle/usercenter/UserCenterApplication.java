94cd953f49f86a53c806ceae8cc112e1eaa87432
Mr.Chen <2490197735@qq.com>
2023-11-09 19:23:33 +0800

    add

--- src/main/java/com/struggle/usercenter/UserCenterApplication.java
+++ src/main/java/com/struggle/usercenter/UserCenterApplication.java
@@ -3,6 +3,7 @@ package com.struggle.usercenter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.struggle.usercenter.mapper")
