fc2c6b5750ddc3997a0518d63a5698d02bbc488d
Mr.Chen <2490197735@qq.com>
2023-11-20 17:57:58 +0800

    add

---
+++ src/main/java/com/struggle/usercenter/model/vo/TeamUserVO.java
@@ -0,0 +1,75 @@
+package com.struggle.usercenter.model.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 队伍和用户信息封装类（脱敏）
+ * @author Mr.Chen
+ */
+@Data
+public class TeamUserVO implements Serializable {
+    private static final long serialVersionUID = -4349417534399440387L;
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 队伍名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 最大人数
+     */
+    private Integer maxNum;
+
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 0 - 公开，1 - 私有，2 - 加密
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    /**
+     * 创建人用户信息
+     */
+    UserVO createUser;
+}
